ebb2fc2663c2c641ad27c487155a772249051976
unknown <gokulvsvikram@gmail.com>
2026-01-24 09:17:29 -0800

    initial commit

---
+++ Lower.java
@@ -0,0 +1,17 @@
+public class Lower {
+    public static void main(String[] args) {
+        byte num1;
+        num1 = 120;
+        System.out.println(num1);
+        // byte can store values upto 127 (1byte) //
+
+        // short can store values from -32,768 to 32,767 (2 bytes) //
+        short num2;
+        num2 = 3567;
+        System.out.println(num2);
+
+        // int numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,807 (4 bytes) //
+        int num3 = 1256985;
+        System.out.println(num3);
+    }
+}
---
+++ hello.java
@@ -0,0 +1,7 @@
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("welcome to java");
+        String myMessage = "hi gokul";
+        System.out.println(myMessage);
+    }
+}
