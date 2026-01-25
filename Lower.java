ead2f0c4b7f9e7122efc128ba8579c1e76867cc6
unknown <gokulvsvikram@gmail.com>
2026-01-25 11:11:22 -0800

    try some other data types

--- Lower.java
+++ Lower.java
@@ -10,8 +10,22 @@ public class Lower {
         num2 = 3567;
         System.out.println(num2);
 
-        // int numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,807 (4 bytes) //
-        int num3 = 1256985;
+        // int numbers from -2,147,483,648 to 2,147,483,647 (4 bytes) //
+        int num3 = 125698;
         System.out.println(num3);
+
+        // long number from -9,233,372,036,854,775,808 to 9,233,372,036,854,807 (8 bytes) //
+        long num4;
+        num4 = 74545448743133L;
+        System.out.println(num4);
+
+        // float decimal numbers with 6 decimal digits (4 bytes) //
+        float num5 = 7.906767f;
+        System.out.println(num5);
+
+        // double decimal numbers with 15 decimal digits (8 bytes) //
+        double num6;
+        num6 = 9.0046445632542d;
+        System.out.println(num6);
     }
 }
