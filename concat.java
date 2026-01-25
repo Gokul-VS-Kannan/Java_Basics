53636af0e8b413c0a05b0768ce3ec4290742a51b
unknown <gokulvsvikram@gmail.com>
2026-01-25 11:50:10 -0800

    Concatenation between string and intiger

---
+++ concat.java
@@ -0,0 +1,11 @@
+public class concat {
+    public static void main(String[] args) {
+        int x = 7;
+        int y = 9;
+
+        System.out.println("The sum is " + x +y); // the + sign after a string means concatination in java //
+        // java evaluvate from left to right //
+
+        System.out.println("The sum is " + (x+y));   // the parantheses ensures addition //
+    }
+}
