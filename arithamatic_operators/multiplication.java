499196f505c6d21e3ee12cc7d4083d8f7e52c5ed
unknown <gokulvsvikram@gmail.com>
2026-02-10 11:47:28 -0800

    multiplying two numbers

---
+++ arithamatic_operators/multiplication.java
@@ -0,0 +1,12 @@
+package arithamatic_operators;
+
+public class multiplication {
+    public static void main(String[] args){
+        int num1 = 5;
+        int num2 = 4;
+
+        int product = num1*num2;
+
+        System.out.println("The product of " + num1 + " and " + num2 + " is : " + product);
+    }
+}
