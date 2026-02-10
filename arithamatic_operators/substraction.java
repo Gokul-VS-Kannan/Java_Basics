fe86947032357d3ba0e6976a9e9e6d122fdaf9b3
unknown <gokulvsvikram@gmail.com>
2026-02-10 11:41:47 -0800

    substracting two numbers

---
+++ arithamatic_operators/substraction.java
@@ -0,0 +1,12 @@
+package arithamatic_operators;
+
+public class substraction {
+    public static void main(String[] args){
+        int num1 = 45;
+        int num2 = 15;
+
+        int difference = num1 - num2;
+
+        System.out.println("The difference of " + num1 + " and " + num2 + " is : " + difference);
+    }
+}
