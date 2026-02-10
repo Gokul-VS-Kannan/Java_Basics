c5026b85c27ea82fe0826403de07a5042d353de9
unknown <gokulvsvikram@gmail.com>
2026-02-10 11:10:37 -0800

    string concatination

---
+++ concat_str.java
@@ -0,0 +1,15 @@
+// concate two string //
+public class concat_str {
+    public static void  main(String[] args){
+        String name;
+        String great;
+
+        name = "Gokul";
+        great = "Hi";
+
+        String result;
+        result = great + " " + name;
+
+        System.out.println(result);
+    }   
+}
