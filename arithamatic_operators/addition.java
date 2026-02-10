338459082ba398f4b57b37d8e86ed8a2a6de59e6
unknown <gokulvsvikram@gmail.com>
2026-02-10 11:27:13 -0800

    adding two numbers

---
+++ arithamatic_operators/addition.java
@@ -0,0 +1,15 @@
+package arithamatic_operators;
+
+public class addition {
+    public static void main(String[] args) {
+        int x;
+        int y;
+
+        x = 5;
+        y = 40;
+
+        int sum = x + y;
+
+        System.out.println("The sum of " + x + " and " + y + " is : " +  sum);
+    }
+}
