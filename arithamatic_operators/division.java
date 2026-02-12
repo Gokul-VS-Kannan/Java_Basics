827f189f0a91f6928ce23b50cdb2069b9fb685f0
unknown <gokulvsvikram@gmail.com>
2026-02-12 11:23:53 -0800

    dividing two inigers

---
+++ arithamatic_operators/division.java
@@ -0,0 +1,16 @@
+package arithamatic_operators;
+
+public class division {
+    public static void main(String[] args){
+        int num1 = 25;
+        int num2 = 5;
+
+        System.out.println(num1/num2);
+        
+        /* when dividing two iniger in java the result will also be an intiger */
+        int num3 = 15;
+        int num4 = 2;
+        System.out.println(num3/num4); // output: 7//
+
+    }    
+}
