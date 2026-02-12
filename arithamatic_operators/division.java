6e7817d7c24cad3639e947c378da5366c4a87d3a
unknown <gokulvsvikram@gmail.com>
2026-02-12 11:29:17 -0800

    dividing two double values

--- arithamatic_operators/division.java
+++ arithamatic_operators/division.java
@@ -12,5 +12,10 @@ public class division {
         int num4 = 2;
         System.out.println(num3/num4); // output: 7//
 
+        /* if we want decimal result use double values */
+        double num5 = 22.0d;
+        double num6 = 7.0d;
+        System.out.println(num5/num6);
+
     }    
 }
