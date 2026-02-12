9ab0ed3f3fae65eeb73590c5683fecc18447bbea
unknown <gokulvsvikram@gmail.com>
2026-02-12 11:01:56 -0800

    narrow casting double to int

---
+++ casting2.java
@@ -0,0 +1,10 @@
+// Narrow casting  or explicite casting//
+public class casting2 {
+   public static void main(String args[]){
+    double mydouble = 5.8960d;
+    int myint = (int)mydouble;
+
+    System.out.println(mydouble);
+    System.out.println(myint);
+   } 
+}
