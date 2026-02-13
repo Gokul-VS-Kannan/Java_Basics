1f97cee0bee6ad3d0589ae93a8a7dad9abcf01f2
unknown <gokulvsvikram@gmail.com>
2026-02-13 12:41:29 -0800

    decrement operation

---
+++ arithamatic_operators/decrement.java
@@ -0,0 +1,12 @@
+package arithamatic_operators;
+
+public class decrement {
+    public static void main(String[] args){
+        int num1 = 6;
+        System.out.println(num1);
+
+        --num1;
+        System.out.println(num1);
+    }
+}
+/* this function simply decrease the value by one */
