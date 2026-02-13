de191f88e2ca224e90665af0d351eb60bac40e30
unknown <gokulvsvikram@gmail.com>
2026-02-13 12:37:32 -0800

    increment operation

---
+++ arithamatic_operators/increment.java
@@ -0,0 +1,13 @@
+package arithamatic_operators;
+
+public class increment {
+    public static void main(String[] args){
+        int num1 = 7;
+        System.out.println(num1);
+
+        ++num1;
+        System.out.println(num1);
+    }
+}
+
+/* this operatin simply increase the value by one */
