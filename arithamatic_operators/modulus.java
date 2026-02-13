7cf2d2296441f57c65486493d2cda5c96f027715
unknown <gokulvsvikram@gmail.com>
2026-02-13 12:32:49 -0800

    modulus operation

---
+++ arithamatic_operators/modulus.java
@@ -0,0 +1,12 @@
+package arithamatic_operators;
+
+public class modulus {
+    public static void main(String[] args){
+        int num1 = 7;
+        int num2 = 4;
+
+        int result = num1%num2;
+
+        System.out.println(result);
+    }
+}
