5184351f285f0e46642d474f3dbdda17aed6ff84
unknown <gokulvsvikram@gmail.com>
2026-02-13 12:53:07 -0800

    addition assignment

---
+++ assignment_operators/sumassign.java
@@ -0,0 +1,11 @@
+package assignment_operators;
+
+public class sumassign {
+    public static void main(String[] args){
+        int n1 = 78;
+        System.out.println(n1);
+
+        n1 += 4;
+        System.out.println(n1);
+    }
+}
