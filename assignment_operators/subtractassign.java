07b3aab6b1f2a43d3ada8eda5a3441317b4ccf4f
unknown <gokulvsvikram@gmail.com>
2026-02-16 12:21:07 -0800

    substraction assignment

---
+++ assignment_operators/subtractassign.java
@@ -0,0 +1,11 @@
+package assignment_operators;
+
+public class subtractassign{
+    public static void main(String[] args){
+        int y = 20;
+        System.out.println(y);
+
+        y -= 5;
+        System.out.println(y);
+    }
+}
