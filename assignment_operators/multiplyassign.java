01eb596dbd058582fb742d193853ab2499c303fd
unknown <gokulvsvikram@gmail.com>
2026-02-17 01:59:19 +0530

    multiplication assignment

---
+++ assignment_operators/multiplyassign.java
@@ -0,0 +1,11 @@
+package assignment_operators;
+
+public class multiplyassign {
+    public static void main(String[] args){
+        int x = 10;
+        System.out.println(x);
+
+        x *=5;
+        System.out.println(x);
+    }
+}
