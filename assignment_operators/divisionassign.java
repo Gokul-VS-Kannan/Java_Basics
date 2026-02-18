e2abe0e43f818c3010026e826cdd0107da22aace
unknown <gokulvsvikram@gmail.com>
2026-02-19 00:55:11 +0530

    division assignment

---
+++ assignment_operators/divisionassign.java
@@ -0,0 +1,13 @@
+package assignment_operators;
+
+public class divisionassign {
+    public static void main(String[] args){
+        int x = 15;
+        System.out.println(x);
+        /* now dividing the value ofo x and store it in x itself */
+
+        x /= 3;
+        System.out.println(x);
+    }
+    
+}
