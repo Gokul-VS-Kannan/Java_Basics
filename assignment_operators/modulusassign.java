f268f92ff040b2f2002c98cd0a70fc124322d791
unknown <gokulvsvikram@gmail.com>
2026-02-19 01:00:42 +0530

    modulus assignment

---
+++ assignment_operators/modulusassign.java
@@ -0,0 +1,11 @@
+package assignment_operators;
+
+public class modulusassign {
+    public static void main(String[] args){
+        int x = 49;
+        System.out.println(x);
+
+        x %=5;
+        System.out.println(x);
+    }
+}
