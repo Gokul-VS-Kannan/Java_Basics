68b3769b01ce2a101a01053432e8cc8c8ba2d4ea
unknown <gokulvsvikram@gmail.com>
2026-02-22 02:49:30 +0530

    equality operation

---
+++ comparison_operators/equaito.java
@@ -0,0 +1,14 @@
+package comparison_operators;
+
+public class equaito {
+    public static void main(String[] args){
+        int x = 7;
+        int y = 5;
+
+        System.out.println(x == y);
+
+        int a = 9;
+        int b = 9;
+        System.out.println(a == b);
+    }
+}
